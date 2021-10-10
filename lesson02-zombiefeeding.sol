456bd0bf643b8ed0d43a07a117da6a642d79dd65
dfassf <dfassf@gmail.com>
2021-10-10 23:32:35 +0900

    lesson2 chapter13

--- lesson02-zombiefeeding.sol
+++ lesson02-zombiefeeding.sol
@@ -25,19 +25,28 @@ contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract = KittyInterface(ckAddress);
 
     // 잘 이해는 안되지만 이걸 바꾸면 뭔가 되겠지..
-    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+    function feedAndMultiply(
+        uint256 _zombieId,
+        uint256 _targetDna,
+        string _species
+    ) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
 
         _targetDna = _targetDna % dnaModulus; //타겟디엔에이를 16자로 제한한다?
         uint256 newDna = (myZombie.dna + _targetDna) / 2;
         // 새로운 DNA는 먹잇감과 기존 좀비의 평균으로 내고 그 DNA를 바탕으로 좀비를 새로 생성
+
+        if (keccak256(_species) == keccak256("kitty")) {
+            newDna = newDna - (newDna % 100) + 99;
+            // newDna
+        }
         _createZombie("NoName", newDna);
     }
 
     function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
         uint256 kittyDna;
         (, , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
