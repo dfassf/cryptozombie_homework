8347819292527b711ec44cf91e0334f9942be2bb
dfassf <dfassf@gmail.com>
2021-10-10 22:27:12 +0900

    lesson2 chapter7

--- lesson02-zombiefeeding.sol
+++ lesson02-zombiefeeding.sol
@@ -2,4 +2,9 @@ pragma solidity ^0.4.19;
 
 import "./lesson02-zombiefactory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+    }
+}
