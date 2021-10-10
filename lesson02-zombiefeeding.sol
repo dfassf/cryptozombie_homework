c13c57a2ef25bdffbf17796441a05321538391d2
dfassf <dfassf@gmail.com>
2021-10-10 22:46:06 +0900

    lesson2 chapter8

--- lesson02-zombiefeeding.sol
+++ lesson02-zombiefeeding.sol
@@ -6,5 +6,9 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+
+        _targetDna = _targetDna % dnaModulus;
+        uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
     }
 }
