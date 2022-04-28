10a20636243c9098ed9d4ba09e140a3e744c6454
Shinwoo Um <dfassf@gmail.com>
2022-04-29 00:02:11 +0900

    chapter5

--- lesson1.sol
+++ lesson1.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.19;
 //2. 여기에 컨트랙트 생성
 contract ZombieFactory {
     uint dnaDigits = 16;
-
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
