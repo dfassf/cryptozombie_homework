5119fc951d0cd0e7e8e93d4739ae588fc2759585
Shinwoo Um <dfassf@gmail.com>
2022-04-29 00:13:50 +0900

    chapter10

--- lesson1.sol
+++ lesson1.sol
@@ -21,7 +21,8 @@ contract ZombieFactory {
     }
 
     function _generateRandomDna(string _str) private view returns(uint) {
-        
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
     }
 
 }
