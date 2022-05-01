8b9276d287fad5404c2987bbd6e9dee6ce4e4b64
Shinwoo Um <dfassf@gmail.com>
2022-05-02 00:04:26 +0900

    chapter2

--- lesson2.sol
+++ lesson2.sol
@@ -14,7 +14,8 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    // 여기서 매핑 선언
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
