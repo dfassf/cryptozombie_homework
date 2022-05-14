32d81d84f2d0cab91a1ebf55a3723d6d109b3370
Shinwoo Um <dfassf@gmail.com>
2022-05-14 13:54:36 +0900

    lesson3 chapter10

--- lesson3/zombiehelper.sol
+++ lesson3/zombiehelper.sol
@@ -18,4 +18,8 @@ contract ZombieHelper is ZombieFeeding {
         require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     }
+
+    function getZombiesByOwner(address _owner) external view returns(uint[]){
+   
+    }
 }
