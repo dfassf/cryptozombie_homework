63adcf963f77bad1edaaba67c35b9ff03a8825e6
Shinwoo Um <dfassf@gmail.com>
2022-06-10 22:28:51 +0900

    lesson5 chapter2

--- lesson5/zombieownership.sol
+++ lesson5/zombieownership.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.19;
 
 import "./zombieattack.sol";
+import "./ERC721.sol";
 
-contract ZombieOwnership is ZombieAttack {
+contract ZombieOwnership is ZombieAttack, ERC721 {
 
 }
